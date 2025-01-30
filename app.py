a39da42fb7a1ffe54fe611ce228bbbb9364e5a93
slacker-devlpr <shubstay@gmail.com>
2025-01-30 13:53:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,68 @@
+# Libraries:
+import streamlit as st
+from openai import OpenAI
+import shelve
+from PIL import Image
+from dotenv import load_dotenv
+import openai
+
+#Hide all unneeded parts of streamlit:
+st.set_page_config(
+    page_title="Shaped-AI, free personal tutor.",
+    page_icon=r"C:\Users\flatd\Downloads\Anka (4).png"
+)
+hide_streamlit_style = """
+<style>
+.css-hi6a2p {padding-top: 0rem;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown('''
+<style>
+.stApp [data-testid="stToolbar"]{
+    display:none;
+}
+</style>
+''', unsafe_allow_html=True)
+enable_scroll = """
+<style>
+.main {
+    overflow: auto;
+}
+</style>
+"""
+st.markdown(enable_scroll, unsafe_allow_html=True)
 
