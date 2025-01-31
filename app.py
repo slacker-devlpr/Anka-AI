a7e7005ac6c7f80c70ccb30b36c4a829c26a69fd
slacker-devlpr <shubstay@gmail.com>
2025-01-31 22:55:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ display_messages(st.session_state.messages)
 # Response mode functions
 def get_system_message():
     mode = st.session_state.mode
-    if mode == "**⚡ Takojšnji odgovor**: Tvoj AI inštruktor bo dal neposredne odgovore na tvoje vprašanje. Ta pristop se osredotoča na zagotavljanje natančnih rešitev z minimalnimi koraki razlage.":
+    if mode == "**⚡ Takojšnji odgovor**":
         return {
             "role": "system",
             "content": (
@@ -263,7 +263,7 @@ def get_system_message():
                 "Be concise. Example: 'Rešitev je $$x = 5$$. Respond in Slovenian unless asked otherwise." "Every math symbol, equation, letter, number has to be incased in $$. For example $$a$$ or $$x + 2$$ Thats how the program knows it has to show it as latex!"
             )
         }
-    elif mode == "**📚 Filozofski način**: Tvoj AI inštruktor te bo vodil skozi probleme z izzivalnimi vprašanji. Ta pristop spodbuja kritično mišljenje in globlje razumevanje konceptov.":
+    elif mode == "**📚 Filozofski način**":
         return {
             "role": "system",
             "content": (
@@ -271,7 +271,7 @@ def get_system_message():
                 "Ask one question at a time. Use LaTeX for all math. Respond in Slovenian unless asked otherwise.""Every math symbol, equation, letter, number has to be incased in $$. For example $$a$$ or $$x + 2$$ Thats how the program knows it has to show it as latex!"
             )
         }
-    elif mode == "**😎 Gen Alpha način**: Fr fr, matematika razložena s strani tvojega giga možganov chad inštruktorja, ki ti dviguje matematično auro, no cap.":
+    elif mode == "**😎 Gen Alpha način**:
         return {
             "role": "system",
             "content": (
