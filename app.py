b9a2f44f6483735abe09900857451d56cfcd1465
slacker-devlpr <shubstay@gmail.com>
2025-01-31 14:19:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ st.sidebar.markdown("""
         .mode-text {
             color: #666666;
             text-align: center;
-            font-family: Fira Code;
+            font-family: Ariel;
             font-size: 14px;
             margin: 10px 0;
         }
