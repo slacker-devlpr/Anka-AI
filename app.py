7ebe7689b4326d678d3c52645d57f561c4de6299
slacker-devlpr <shubstay@gmail.com>
2025-01-31 18:31:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -158,6 +158,8 @@ st.sidebar.markdown('<hr class="sidebar-divider">', unsafe_allow_html=True)
 # Add About button
 if st.sidebar.button("About", key = "about"):
     @st.dialog("i")
+    return
+
 USER_AVATAR = "👤"
 BOT_AVATAR = r"top-logo.png"
 client = OpenAI(api_key='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')
