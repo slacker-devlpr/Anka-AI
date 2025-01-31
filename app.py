e5301c7cd044b229f1884e7255da9d2e7272cf2f
slacker-devlpr <shubstay@gmail.com>
2025-01-31 16:39:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ if prompt := st.chat_input("How can I help?"):
 
     # Show "thinking" animation with fade in and out
     thinking_message = st.empty()
-    thinking_message.markdown('<div class="fade-in-out">    Razmišljam...</div>', unsafe_allow_html=True)
+    thinking_message.markdown('<div class="fade-in-out">‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ Razmišljam...</div>', unsafe_allow_html=True)
 
     system_message = {
         "role": "system",
