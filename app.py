d20dfff6a97197c980651fc78226285a252a69b7
slacker-devlpr <shubstay@gmail.com>
2025-02-08 12:52:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -392,11 +392,11 @@ if prompt := st.chat_input("Kako lahko pomagam?"):
 if st.session_state.get("generate_response"):
     with st.spinner("Razmišljam..."):
         try:
-            @st.dialog("⚠️🚧 OPOZORILO: Težave s strežniki🚧⚠️")
+            #@st.dialog("⚠️🚧 OPOZORILO: Težave s strežniki🚧⚠️")
             def vote1():
                 st.write("Zaradi hitrega povečanja priljubljenosti platforme DeepSeek se trenutno soočajo z velikimi težavami s strežniki. Posledično ima tudi Shaped AI matematični inštruktor, ki deluje s pomočjo DeepSeeka, tehnične težave.") 
                 st.write("🔧 Ekipa intenzivno dela na odpravi težav, vendar to lahko začasno vpliva na hitrost odzivanja in delovanje storitve. Hvala za vaše razumevanje in potrpežljivost! 🔧")
-            vote1()
+            #vote1()
             # Use the DeepSeek API to generate the chat completion
             response = client.chat.completions.create(
                 model=st.session_state["openai_model"],
