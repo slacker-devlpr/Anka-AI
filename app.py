682dbf7ad2678cff89a3e327abeb1a9221f03df7
slacker-devlpr <shubstay@gmail.com>
2025-02-24 19:18:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -222,6 +222,7 @@ if st.session_state.get("show_camera_dialog", False):
             st.rerun()
 
     handle_camera_dialog()
+    st.rerun
 
 # Process image after dialog closes
 if st.session_state.get("processing_image", False):
