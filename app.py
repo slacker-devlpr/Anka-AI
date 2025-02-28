945642faf630b15f6bd103a23e6328b33dee7c92
slacker-devlpr <shubstay@gmail.com>
2025-02-28 17:38:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ if st.session_state.get("processing_image", False):
             # Check if Gemini returned an error message
             if "#error.user#" in extracted_problem:
                 st.session_state.messages.append({"role": "error", "content": "Gemini Vision ni našel naloge v vaši sliki."})
-                st.session_state.generate_response = False
+                st.session_state.generate_response = True
 
                 st.rerun()
             else:
