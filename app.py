0ec106a77e5c6519419d4b9ec099c2e8b368c0b0
slacker-devlpr <shubstay@gmail.com>
2025-03-01 12:09:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,20 +94,7 @@ length_captcha = 4
 width = 200
 height = 150
 
-# Language selection dialog
-if 'language' not in st.session_state:
-    
-    st.image("shaped-ai.png")
-    st.write("Which ShapedAI version would you like to use?/Katero verzijo ShapedAI bi rad uporabljav?")
-    col1, col2 = st.columns(2)
-    if col1.button("English", key="english" ):
-        st.session_state.language = "English"
-        st.rerun()
-    if col2.button("Slovene", key="slovene"):
-        st.session_state.language = "Slovene"
-        st.rerun()
-    st.stop()  
-    
+st.session_state.language = "Slovene"
 # Define the function for CAPTCHA control
 def captcha_control():
     if 'controllo' not in st.session_state or st.session_state['controllo'] == False:
@@ -158,6 +145,20 @@ def captcha_control():
         else:
             # Wait for the button click
             st.stop()
+# Language selection dialog
+if 'language' not in st.session_state:
+    @st.dialog("Select Language / Izberite Jezik")
+    def language_dialog():
+        st.write("Please select your language / Prosimo, izberite svoj jezik")
+        col1, col2 = st.columns(2)
+        if col1.button("English"):
+            st.session_state.language = "English"
+            st.rerun()
+        if col2.button("Slovene"):
+            st.session_state.language = "Slovene"
+            st.rerun()
+    language_dialog()
+    st.stop()  # Stop execution until language is selected
 
 # Main logic
 if 'controllo' not in st.session_state or st.session_state['controllo'] == False:
