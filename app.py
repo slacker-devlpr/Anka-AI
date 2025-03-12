8ab63fffed9a0bd1f40a7784493454003ddd8c4d
slacker-devlpr <shubstay@gmail.com>
2025-03-12 18:01:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from streamlit_cropper import st_cropper
 
 # Page config:
 st.set_page_config(
-    page_title="Shaped AI, Osebni Inštruktor Matematike",
+    page_title="ShapedAI, personal math tutor",
     page_icon=r"shaped-logo.png"
 )
 
