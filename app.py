e6b2cb82f5ce71b2a4f0d22de49c9c273d85c395
slacker-devlpr <shubstay@gmail.com>
2025-03-12 16:52:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.set_page_config(
     page_icon=r"shaped-logo.png"
 )
 
-st.markdown(<meta name="google-site-verification" content="HhVBA6H2LDsstYgjPmDMh99HdGQAgVRpQ84U9Nv4GFw" />)
+st.markdown("<meta name="google-site-verification" content="HhVBA6H2LDsstYgjPmDMh99HdGQAgVRpQ84U9Nv4GFw" />")
 # Load css from assets
 def load_css(file_path):
     with open(file_path) as f:
